7559ca3fa2690e109a3c24aa3f2b0d9b62b595b7
rubyongainz <rubyongainz@gmail.com>
2023-08-27 15:59:56 +0300

    some images added

---
+++ pages/auth.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black opacity-70 w-full h-full "></div>
+    </div>
+  );
+};
+
+export default Auth;
